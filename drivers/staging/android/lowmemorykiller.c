baf37fd7b6774c6f1b74297f4d3a64e7269c6c3b
wds <103055143+wds204@users.noreply.github.com>
2026-05-24 23:53:42 +0700

    Increase lowmem_adj_size and lowmem_minfree array size

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -75,15 +75,18 @@ static short lowmem_adj[6] = {
 	12,
 };
 
-static int lowmem_adj_size = 4;
+static int lowmem_adj_size = 6;
 static int lowmem_minfree[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	16 * 1024,	/* 64MB */
+	3 * 512,   /* 6MB  - foreground */
+	2 * 1024,  /* 8MB */
+	4 * 1024,  /* 16MB */
+	16 * 1024, /* 64MB */
+	24 * 1024, /* 96MB */
+	30 * 1024, /* 120MB - kill background */
+};
 };
 
-static int lowmem_minfree_size = 4;
+static int lowmem_minfree_size = 6;
 static int lmk_fast_run = 1;
 
 static unsigned long lowmem_deathpending_timeout;
